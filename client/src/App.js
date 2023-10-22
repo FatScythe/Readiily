7441d7858dc0c008903ceb26ac3e3ac60985c14e
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-10-22 18:20:59 +0100

    Added Arrow icon and mount auth page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 // Pages
-import Homepage from "./pages/home";
+// import Homepage from "./pages/home";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
     <div className='App overflow-hidden'>
-      <Homepage />
+      {/* <Homepage /> */}
+      <Auth />
     </div>
   );
 }
