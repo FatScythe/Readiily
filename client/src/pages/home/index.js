5ff9522071859afd78008a08e2cc020cf2dfe8b8
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-10-22 12:21:45 +0100

    Included CTA and Footer

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,13 +1,17 @@
 // Components
 import Header from "../../components/header";
 import Features from "../../components/features";
+import Cta from "../../components/features/cta";
+import Footer from "../../components/footer";
 
 const Homepage = () => {
   return (
-    <>
+    <main>
       <Header />
       <Features />
-    </>
+      <Cta />
+      <Footer />
+    </main>
   );
 };
 
