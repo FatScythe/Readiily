543f3fd0cb1b6c55b59a5b8b381bb700e5c2ee86
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-10-23 13:24:38 +0100

    Added routing and Toastify

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 // Pages
-// import Homepage from "./pages/home";
+import Homepage from "./pages/home";
 import Auth from "./pages/Auth";
+// Toastify
+import Toast from "./components/toast";
+// Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  { path: "/auth", element: <Auth /> },
+]);
 function App() {
   return (
     <div className='App overflow-hidden'>
-      {/* <Homepage /> */}
-      <Auth />
+      <Toast />
+      <RouterProvider router={router} />
     </div>
   );
 }
