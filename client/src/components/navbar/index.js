286a28caf26c19f93ff3a11b51872a1b536064e0
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-10-23 13:22:26 +0100

    Added Links and made minor fixes

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,5 +1,6 @@
 import "./navbar.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 // Images
 import logo from "../../assets/images/Rlogo.png";
 import icon from "../../assets/images/R-light.png";
@@ -9,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between items-center container relative py-3 md:pt-0'>
       <div className='flex w-full fixed top-1 md:static md:w-fit md:block justify-between items-center z-30'>
-        <div className='basis-1/3'>
+        <Link to='/' className='basis-1/3'>
           <img
             src={logo}
             className='w-30 h-20 hidden md:block'
@@ -20,7 +21,7 @@ const Navbar = () => {
             className='w-10 sm:w-16 h-10 sm:h-14 md:hidden'
             alt='readiily'
           />
-        </div>
+        </Link>
 
         <button
           className={`toggle-menu ${isNavOpen ? "open" : "close"}`}
@@ -32,26 +33,39 @@ const Navbar = () => {
       </div>
 
       <div
+        onClick={() => setIsNavOpen(false)}
         className={`block pt-40 text-center md:pt-0 text-white sm:text-black bg-black z-10 md:bg-transparent transition-all duration-700 fixed w-full ${
           isNavOpen ? "right-0" : "-right-full"
         } top-0 bottom-0 md:static md:flex justify-between items-center basis-4/6`}
       >
         <ul className='flex flex-col gap-10 md:flex-row items-center md:gap-4 cursor-pointer basis-1/2'>
-          <li className='text-xl sm:text-2xl md:text-base font-semibold'>
+          <a
+            href='#features'
+            className='text-xl sm:text-2xl md:text-base font-semibold hover:text-orange transition-colors duration-500'
+          >
             Features
-          </li>
-          <li className='text-xl sm:text-2xl md:text-base font-semibold'>
+          </a>
+          <a
+            href='#cta'
+            className='text-xl sm:text-2xl md:text-base font-semibold hover:text-orange transition-colors duration-500'
+          >
             Pricing
-          </li>
+          </a>
         </ul>
 
         <div className='flex flex-col pt-10 md:pt-0 gap-10 md:flex-row justify-end items-center md:gap-0 basis-1/2'>
-          <button className='block w-fit mx-auto md:w-full md:inline-block text-xl sm:text-2xl md:text-base font-semibold'>
+          <Link
+            to='/auth'
+            className='block w-fit mx-auto md:w-full md:inline-block text-xl sm:text-2xl md:text-base font-semibold hover:text-orange transition-colors duration-500'
+          >
             Log in
-          </button>
-          <button className='font-semibold text-white text-xl sm:text-2xl md:text-sm bg-secondary px-3 py-2 rounded-md md:w-full'>
+          </Link>
+          <Link
+            to='/auth?signup=true'
+            className='font-semibold text-white text-xl sm:text-2xl md:text-sm bg-secondary px-3 py-2 rounded-md md:w-full hover:scale-105 transition-all duration-500'
+          >
             Get your first design!
-          </button>
+          </Link>
         </div>
       </div>
     </nav>
