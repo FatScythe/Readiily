9d406a4ccd4dff94b0127fd3cebaa52f669b8508
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-10-24 16:58:58 +0100

    Added Route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 // Pages
-import Homepage from "./pages/home";
+import Homepage from "./pages/Home";
 import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import HomeDash from "./pages/Dashboard/pages/home";
 // Toastify
 import Toast from "./components/toast";
 // Router
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Calendar from "./pages/Dashboard/pages/calendar";
+import Account from "./pages/Dashboard/pages/account";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +21,33 @@ const router = createBrowserRouter([
     element: <Homepage />,
   },
   { path: "/auth", element: <Auth /> },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to='/dashboard/home' />,
+      },
+      {
+        path: "home",
+        element: <HomeDash />,
+      },
+      {
+        path: "calendar",
+        element: <Calendar />,
+      },
+      {
+        path: "idea",
+        element: <h1 className='text-center text-2xl mt-10'>Coming soon!!!</h1>,
+      },
+      {
+        path: "account",
+        element: <Account />,
+      },
+    ],
+  },
+  { path: "*", element: <div>Page Not Found : ) 404</div> },
 ]);
 function App() {
   return (
