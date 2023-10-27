f34a241125360064dfd353e65a54fada5f78887e
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-10-27 22:51:02 +0100

    Added new user page for dashboard

---
+++ client/src/pages/Dashboard/pages/user/ideate/index.js
@@ -0,0 +1,11 @@
+const Ideate = () => {
+  return (
+    <section>
+      <h2 className='text-4xl sm:text-6xl md:text-7xl text-center my-20 font-semibold'>
+        Coming Soon !!!
+      </h2>
+    </section>
+  );
+};
+
+export default Ideate;
