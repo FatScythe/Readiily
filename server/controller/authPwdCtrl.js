661c7b85f899d35df8e4e875a4aed632d218fc14
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-04 22:19:54 +0100

    Throwing proper Errors

--- server/controller/authPwdCtrl.js
+++ server/controller/authPwdCtrl.js
@@ -9,32 +9,23 @@ const register = async (req, res) => {
   if (!name || !email || !password) {
     throw new BadRequestError("Please fill all fields");
   }
-  if (password.length < 6) {
-    throw new BadRequestError("Password should be minimum of 6 character");
+  if (password.length < 8) {
+    throw new BadRequestError("Password should be minimum of 8 character");
   }
   const isFirstAccount = (await Account.countDocuments({})) === 0;
   const role = isFirstAccount ? "admin" : "user";
 
-  const account = await Account.create({
+  await Account.create({
     name,
     password,
     email,
     role,
   });
 
-  res.status(StatusCodes.CREATED).json({ account });
+  res.status(StatusCodes.CREATED).json({ msg: "Account sucessfully created" });
 };
 
 const login = async (req, res) => {
-  const { password } = req.body;
-
-  if (!password) {
-    throw new BadRequestError("Please provide password");
-  }
-  if (password.length < 6) {
-    throw new BadRequestError("Password should be minimum of 6 character");
-  }
-
   attachCookieToResponse(res, req.user);
   res.status(StatusCodes.OK).json(req.user);
 };
