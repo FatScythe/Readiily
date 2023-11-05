ca1f80692140c781d9f8e71ca278537a570410da
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-05 18:13:16 +0100

    Added FetchAccount fn

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,44 @@
 import "./App.css";
+import { useEffect } from "react";
 // Toastify
 import Toast from "./components/toast";
 // Router
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+// Redux
+import { useDispatch } from "react-redux";
+import { saveAccount, removeAccount } from "./features/auth/authSlice";
+// Utils
+import url from "./utils/url";
+
 function App() {
+  const dispatch = useDispatch();
+
+  const fetchAccount = async () => {
+    try {
+      const response = await fetch(url + "/api/v1/account/showMe");
+      if (!response.ok) {
+        dispatch(removeAccount());
+      }
+      const data = await response.json();
+
+      if (data?.msg) {
+        console.error(data.msg);
+        dispatch(removeAccount());
+        return;
+      }
+
+      dispatch(saveAccount(data));
+    } catch (error) {
+      console.error(error);
+      dispatch(removeAccount());
+    }
+  };
+
+  useEffect(() => {
+    fetchAccount();
+  });
+
   return (
     <div className='App overflow-hidden'>
       <Toast />
