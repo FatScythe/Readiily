416a38bfcd20820795ba0fd5a5c9085b422023b2
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-08 19:36:53 +0100

    Added link to the overview page

--- client/src/pages/Dashboard/pages/user/home/index.js
+++ client/src/pages/Dashboard/pages/user/home/index.js
@@ -1,30 +1,56 @@
+import { Link } from "react-router-dom";
 // Hook
 import useTitle from "../../../../../hooks/useTitle";
+// Redux
+import { useSelector } from "react-redux";
 
 const UserHome = () => {
   useTitle("Overview");
+  const { account } = useSelector((store) => store.auth);
+  const { brands } = useSelector((store) => store.brand);
+
   return (
     <section className='mb-20'>
       <div className='p-2 sm:p-3 md:p-4 w-full sm:w-11/12 mx-auto flex flex-col justify-start gap-4 items-center'>
         <h2 className='text-blue text-xl sm:text-3xl md:text-4xl font-semibold md:font-bold text-center'>
-          Welcome, Ayomide
+          Welcome, {account?.name.split(" ")[0] || "User"}
         </h2>
         <div className='w-full h-80 bg-slate-300 mx-auto rounded-md'></div>
         <div className='flex flex-col sm:flex-row justify-between items-center gap-6 w-full'>
-          <div className='bg-white/90 w-full sm:w-3/4 gap-1 sm:gap-3 shadow-md hover:shadow-lg text-blue py-10 px-8 h-full rounded-md text-xl flex flex-col justify-between items-center'>
-            <span>1</span> <span>Brands</span>
-          </div>
-          <div className='bg-white/90 w-full sm:w-3/4 gap-1 sm:gap-3 shadow-md hover:shadow-lg text-blue py-10 px-8 h-full rounded-md text-xl flex flex-col justify-between items-center'>
+          <Link
+            to='/dashboard/account/brand/create'
+            className='bg-white/90 w-full sm:w-3/4 gap-1 sm:gap-3 shadow-md hover:shadow-lg text-blue py-10 px-8 h-full rounded-md text-xl flex flex-col justify-between items-center'
+          >
+            {brands ? (
+              <>
+                <span>{brands.nb}</span> <span>Brands</span>
+              </>
+            ) : (
+              <>
+                <span>...</span> <span>Brands</span>
+              </>
+            )}
+          </Link>
+          <Link
+            to='/dashboard/account/wallet'
+            className='bg-white/90 w-full sm:w-3/4 gap-1 sm:gap-3 shadow-md hover:shadow-lg text-blue py-10 px-8 h-full rounded-md text-xl flex flex-col justify-between items-center'
+          >
             <span>My</span> <span>Wallet</span>
-          </div>
-          <div className='bg-white/90 w-full sm:w-3/4 gap-1 sm:gap-3 shadow-md hover:shadow-lg text-blue py-10 px-8 h-full rounded-md text-xl flex flex-col justify-between items-center'>
+          </Link>
+          <Link
+            to='/dashboard/calendar'
+            className='bg-white/90 w-full sm:w-3/4 gap-1 sm:gap-3 shadow-md hover:shadow-lg text-blue py-10 px-8 h-full rounded-md text-xl flex flex-col justify-between items-center'
+          >
             <span>0</span>
             <span>Comments</span>
-          </div>
-          <div className='bg-white/90 w-full sm:w-3/4 gap-1 sm:gap-3 shadow-md hover:shadow-lg text-blue py-10 px-8 h-full rounded-md text-xl flex flex-col justify-between items-center'>
+          </Link>
+          <Link
+            to='/dashboard/account/ticket/tickets'
+            className='bg-white/90 w-full sm:w-3/4 gap-1 sm:gap-3 shadow-md hover:shadow-lg text-blue py-10 px-8 h-full rounded-md text-xl flex flex-col justify-between items-center'
+          >
             <span>2</span>
             <span>Tickets</span>
-          </div>
+          </Link>
         </div>
       </div>
     </section>
