27cea58994a1131a8ab489478abb82eaafc0f1f1
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-08 22:25:05 +0100

    Added State management for requests

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/auth/authSlice";
 import brandSlice from "./features/brand/brandSlice";
+import requestSlice from "./features/request/requestSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     brand: brandSlice,
+    request: requestSlice,
   },
 });
