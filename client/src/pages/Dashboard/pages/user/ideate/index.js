9baf04b01118ac857625bd0c92a2a3aed41c33f4
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-10 08:14:39 +0100

    Added Figma UI changes

--- client/src/pages/Dashboard/pages/user/ideate/index.js
+++ client/src/pages/Dashboard/pages/user/ideate/index.js
@@ -1,9 +1,15 @@
 const Ideate = () => {
   return (
     <section>
-      <h2 className='text-4xl sm:text-6xl md:text-7xl text-center my-20 font-semibold'>
-        Coming Soon !!!
-      </h2>
+      <header className='text-xl sm:text-3xl p-4 font-semibold bg-primary'>
+        Ideas for next post ?
+      </header>
+
+      <main className='grid place-items-center h-[80vh]'>
+        <h3 className='text-2xl sm:text-4xl text-gray-400 font-extrabold'>
+          Coming Soon!
+        </h3>
+      </main>
     </section>
   );
 };
