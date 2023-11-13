587dd2b38688ce9ba944a8d9ba6551b1dc6b1b91
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-13 19:37:09 +0100

    Added: State Management for ticket

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/auth/authSlice";
 import brandSlice from "./features/brand/brandSlice";
 import requestSlice from "./features/request/requestSlice";
+import ticketSlice from "./features/ticket/ticketSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     brand: brandSlice,
     request: requestSlice,
+    ticket: ticketSlice,
   },
 });
