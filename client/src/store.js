1dfc0dac24762984f9713c0b8ad8d93fd39f9b54
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-14 19:38:19 +0100

    Added: State mangement for comment

--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/auth/authSlice";
 import brandSlice from "./features/brand/brandSlice";
 import requestSlice from "./features/request/requestSlice";
+import commentSlice from "./features/comment/commentSlice";
 import ticketSlice from "./features/ticket/ticketSlice";
 
 export const store = configureStore({
@@ -9,6 +10,7 @@ export const store = configureStore({
     auth: authSlice,
     brand: brandSlice,
     request: requestSlice,
+    comment: commentSlice,
     ticket: ticketSlice,
   },
 });
