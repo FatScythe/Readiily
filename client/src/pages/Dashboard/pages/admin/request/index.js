4b9bdc0de036fc53ed4a5c400cee54ff795a4a7e
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-19 21:43:23 +0100

    Fixed styling

--- client/src/pages/Dashboard/pages/admin/request/index.js
+++ client/src/pages/Dashboard/pages/admin/request/index.js
@@ -52,13 +52,14 @@ const Request = () => {
         <header>
           <div className='text-blue font-bold text-lg sm:text-xl py-4 flex justify-between items-center'>
             <span>Unassigned Design Request</span>
-            <button
+            <Link
+              to='/dashboard/create'
               className='font-normal self-end flex justify-start items-center gap-2 bg-black text-white px-4 py-2 rounded-3xl'
               title='create designer'
             >
               <span className='hidden sm:block'>Create designer </span>
               <AddUserIcon className='w-6 h-6 stroke-2' />
-            </button>
+            </Link>
           </div>
           {unassignedReq.length >= 1 && (
             <div className='grid grid-cols-12 py-3 px-2 sm:py-6 sm:px-5 bg-blue/10 gap-2'>
@@ -181,7 +182,7 @@ const SingleRequest = ({ request, requestId, setRequestId, setView }) => {
 
 const RequestModal = ({ view, setView }) => {
   return (
-    <div className='fixed top-0 right-0 left-0 bottom-0 bg-black/5'>
+    <div className='fixed top-0 right-0 left-0 bottom-0 bg-black/5 z-20'>
       <div className='absolute h-4/6 -bottom-64 left-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-1/2 md::w-1/3 bg-grayish p-2 rounded-xl'>
         <div className='w-full flex justify-between items-center my-3'>
           <button
