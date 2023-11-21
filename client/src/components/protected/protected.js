d6b743e52184bee3524236d8a655f3f7e5cbfc78
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-11-21 09:06:20 +0100

    Added Loader

--- client/src/components/protected/protected.js
+++ client/src/components/protected/protected.js
@@ -1,6 +1,6 @@
-// import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+// Component
 import Loader from "../loader";
 
 const Protected = ({ children }) => {
